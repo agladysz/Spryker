ba459e8b9b168ad53152046df64fb7752a97b0a6
aleksandra.gladysz <aleksandra.gladysz@accenture.com>
2022-08-06 19:48:31 +0200

    Architecture 2 HelloSpryker - Zend

---
+++ src/Pyz/Yves/HelloSpryker/Controller/IndexController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Pyz\Yves\HelloSpryker\Controller;
+
+use Spryker\Yves\Kernel\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @method \Pyz\Client\HelloSpryker\HelloSprykerClientInterface getClient()
+ */
+class IndexController extends AbstractController
+{
+    /**
+     * @param Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Spryker\Yves\Kernel\View\View
+     */
+    public function indexAction(Request $request)
+    {
+        $data = ['reversedString' => 'Hello Spryker!'];
+
+        return $this->view(
+            $data,
+            [],
+            '@HelloSpryker/views/index/index.twig'
+        );
+    }
+}
